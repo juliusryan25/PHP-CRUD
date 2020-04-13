47ec1358c11e9aefdca88e69d2b75a0c5df0d28a
juliusryan25 <63332117+juliusryan25@users.noreply.github.com>
2020-04-13 16:07:33 +0700

    Delete tampil.php

--- tampil.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include('lib/koneksipdo.php');
-    $result = $con-> query('SELECT * FROM `databarang`');
-    echo "<table border=1>";
-    echo "<th>Username</th>";
-    foreach($result as $d) : echo "<tr><td>".$d['kode_produk']."</td></tr>";
-    endforeach;
-    echo "</table>";
-?>
